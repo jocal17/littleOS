8a9d46194b0917798bbd04441e75d956d64e72ac
Joe <CalivaJoseph@gmail.com>
2018-02-27 12:39:58 -0500

    Update loader.s

--- loader.s
+++ loader.s
@@ -1,27 +1,20 @@
-	      global loader                   ; the entry symbol for ELF
+	global loader                    ; the entry symbol for ELF
 
         section .data
 
-<<<<<<< HEAD
-        MAGIC_NUMBER equ 0x1BADB002     ; define the magic number constant
-	      FLAGS        equ 0x0            ; multiboot flags
-	      CHECKSUM     equ -MAGIC_NUMBER  ; calculate the checksum
-	                                      ; (magic number + checksum + flags should equal 0)
-=======
-section .text                   ; start of the text (code) section
-align 4                         ; the code must be 4 byte aligned
-    dd MAGIC_NUMBER             ; write the magic number to the machine code,
-    dd FLAGS                    ; the flags,
-    dd CHECKSUM                 ; and the checksum
->>>>>>> 063acd9d89bf36753b4f6bd34e63d0813acd376d
 
-	      section .text                   ; start of the text (code) section
-	      align 4                         ; the code must be 4 byte aligned
-	      dd MAGIC_NUMBER                 ; write the magic number to the machine code,
-	      dd FLAGS                        ; the flags,
-	      dd CHECKSUM                     ; and the checksum
+        MAGIC_NUMBER equ 0x1BADB002     ; define the magic number constant
+	FLAGS        equ 0x0            ; multiboot flags
+	CHECKSUM     equ -MAGIC_NUMBER  ; calculate the checksum
+	                                ; (magic number + checksum + flags should equal 0)
+        section .text                   ; start of the text (code) section
+       
+        align 4                         ; the code must be 4 byte aligned
+        dd MAGIC_NUMBER                 ; write the magic number to the machine code,
+        dd FLAGS                        ; the flags,
+        dd CHECKSUM                     ; and the checksum
 
 loader:                                 ; the loader label (defined as entry point in linker script)
-	      mov eax, 0xCAFEBABE             ; place the number 0xCAFEBABE in the register eax
+	mov eax, 0xCAFEBABE             ; place the number 0xCAFEBABE in the register eax
 loop:
-	      jmp loop                        ; loop forever
+	 mp loop                        ; loop forever
