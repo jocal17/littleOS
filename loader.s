790b503f559540bbaad8b7908350d9e4651fb755
jocal17 <calivajoseph@gmail.com>
2018-03-18 12:33:20 -0400

    formatting

--- loader.s
+++ loader.s
@@ -1,30 +1,33 @@
-  global loader                   ; the entry symbol for ELF
+        global loader
+        extern sum_of_three
 
-  MAGIC_NUMBER equ 0x1BADB002     ; define the magic number constant
-  FLAGS        equ 0x0            ; multiboot flags
-  CHECKSUM     equ -MAGIC_NUMBER  ; calculate the checksum
-                                  ; (magic number + checksum + flags should equal 0)
-  KERNEL_STACK_SIZE equ 4096      ; size of stack in bytes
-
-  section .bss
-  align 4                         ; align at 4 bytes
-
-  kernel_stack:                   ; label points to beginning of memory
-      resb KERNEL_STACK_SIZE      ; reserve stack for the kernel
-
-  section .text:                  ; start of the text (code) section
-  align 4                         ; the cglobal loader
+        MAGIC_NUMBER equ 0x1BADB002
+        FLAGS        equ 0x0
+        CHECKSUM     equ -MAGIC_NUMBER
 
 
-  loader:                         ; the loader label (defined as entry point in linker script)
-      mov esp, kernel_stack + KERNEL_STACK_SIZE       ; point esp to the start of the
-                                                                                      ;stack (end of memory area)
-      extern sum_of_three     ; the function sum_of_three is defined elsewhere
-      push dword 3            ; arg3
-      push dword 2            ; arg2
-      push dword 1            ; arg1
-      call sum_of_three       ; call the function, the result will be in eax
+        section .text
+        align 4
+        dd MAGIC_NUMBER
+        dd FLAGS
+        dd CHECKSUM
 
-  .loop:
-      jmp .loop               ; loop forever
+loader:
+        KERNEL_STACK_SIZE equ 4096
+        mov esp, kernel_stack + KERNEL_STACK_SIZE
+        push dword 3
+        push dword 2
+        push dword 1
+        call sum_of_three
+
+.loop:
+        jmp .loop
+
+
+        section .bss
+        align 4
+
+kernel_stack:
+        resb KERNEL_STACK_SIZE
+
 
