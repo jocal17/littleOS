b9e18a5fe481f0d8a8626ce3cf355806791d1752
jocal17 <calivajoseph@gmail.com>
2018-05-10 15:08:51 -0400

    stdint.h

---
+++ stdint.h
@@ -0,0 +1,10 @@
+#ifndef STD_INT_H
+#define STD_INT_H
+
+//When compiled with GCC on a 32 bit system.
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+typedef unsigned long long uint64_t;
+
+#endif
